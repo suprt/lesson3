68a16a1a43af78a7c611548089dc01b0a37c025b
Egor <555666741@mail.ru>
2026-09-23 20:15:04 +0500

    sync/pool/task1

---
+++ sync/pool/task1/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var bufPool = sync.Pool{
+	New: func() any {
+		return new([]byte)
+	},
+}
+
+func ProcessString(s string) string {
+	buf := bufPool.Get().(*[]byte)
+	*buf = (*buf)[:0]
+	for _, v := range s {
+		if v >= 'a' && v <= 'z' {
+			v = v - ('a' - 'A')
+		}
+		*buf = append(*buf, byte(v))
+	}
+	res := string(*buf)
+
+	bufPool.Put(buf)
+
+	return res
+}
+
+func AltProcessString(s string) string {
+	buf := []byte(s)
+	for i, v := range s {
+		if v >= 'a' && v <= 'z' {
+			buf[i] -= 'a' - 'A'
+		}
+
+	}
+	return string(buf)
+
+}
+
+func main() {
+	examples := []string{
+		"hello, world!",
+		"gopher",
+		"lorem ipsum dolor sit amet",
+	}
+
+	for _, s := range examples {
+		processed := ProcessString(s)
+		fmt.Printf("Original: %q\nProcessed: %q\n\n", s, processed)
+	}
+
+}
---
+++ sync/pool/task1/proccess_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"strings"
+	"testing"
+)
+
+var result string
+
+func TestAllocations(t *testing.T) {
+	s := strings.Repeat("hello world ", 1000)
+
+	allocsPool := testing.AllocsPerRun(100, func() {
+		for i := 0; i < 1000; i++ {
+			result = ProcessString(s)
+		}
+	})
+
+	allocsNoPool := testing.AllocsPerRun(100, func() {
+		for i := 0; i < 1000; i++ {
+			result = AltProcessString(s)
+		}
+	})
+
+	t.Logf("Pool: %.0f allocations", allocsPool)
+	t.Logf("No Pool: %.0f allocations", allocsNoPool)
+}
